2248dcef96530a5dcacb888e2933ec7b60fd3469
Theo <theomhalpern@gmail.com>
2024-03-30 20:35:56 -0400

    fixed the compensts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({children,}: Readonly<{
 }>) {
     return (
         <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className+ " bg-white bg-cover"}>{children}</body>
         </html>
     );
 }
